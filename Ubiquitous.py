48a4cd46906d0b7c854af0e50396666a14617413
Andrew Dixon <PsychoCoderMonkey@gmail.com>
2026-02-07 22:47:31 -0600

    Refactors model management and output directories
    - Adds `getOutputDir` function to manage script output directory creation and ignores.
    - Updates `getModel` to download models from Hugging Face with caching and overriding options.
    - Consolidates global paths (`MODEL_DIR`, `OUTPUT_DIR`) under functions for dynamic generation.
    
    Relates to improvement in model handling and organized script outputs

--- Ubiquitous.py
+++ Ubiquitous.py
@@ -12,6 +12,15 @@ from pathlib import Path
 
 
 def getModelDir() -> Path:
+  """
+  Build and return the directory where models will be stored.
+
+  Generates a .gitignore file to retrain the directory and ignore anything put within.
+
+  :return: Model working directory path
+  :rtype: Path
+  """
+
   # Build the path for where we want to store models.
   base = Path(__file__).parent
   modelDir = base / 'models'
@@ -26,11 +35,42 @@ def getModelDir() -> Path:
   # Return the path after we have made sure it exists and enforced the gitignore file.
   return Path(modelDir)
 
-MODEL_DIR = getModelDir()
+
+def getOutputDir() -> Path:
+  """
+  Build and return the directory for output of the scripts.
+
+  Generates a .gitignore file to retain the directory and ignore anything put within.
+
+  :return: Direcotry used for easy collection of output from scripts.
+  :rtype: Path
+  """
+  # Build the path for where to save output.
+  base = Path(__file__).parent
+  outputDir = base / 'output'
+
+  outputDir.mkdir(parents=True, exist_ok=True)
+  gitignore = '*\n!.gitignore'
+  with (outputDir / '.gitignore').open('w') as f:
+    f.write(gitignore)
+
+  # Return the path after we have made sure it exists and enforced the gitignore file.
+  return Path(outputDir)
 
 
 def getModel(repoID: str = None, fileName: str = None, override: bool = False) -> bool:
+  """
+  Download a model from hugging face repo and save it to a local directory.
 
+  :param repoID: Repository ID of model to download from hugging face
+  :type repoID: str
+  :param fileName: Filename of model to download from hugging face repo.
+  :type fileName: str
+  :param override: Force delete and redownload of a model (true/false)
+  :type override: bool
+  :return: Was script successful?
+  :rtype: bool
+  """
   from huggingface_hub import hf_hub_download
 
   # Dump out if we didn't get the info we need to download the model.
@@ -51,3 +91,8 @@ def getModel(repoID: str = None, fileName: str = None, override: bool = False) -
   )
 
   return modelPath is not None
+
+
+# Globals
+MODEL_DIR = getModelDir()
+OUTPUT_DIR = getOutputDir()

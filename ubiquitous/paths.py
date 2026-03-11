31cbfb8a3a9cb8cdb7fba06dadb1886c0c635df5
Andrew Dixon <PsychoCoderMonkey@gmail.com>
2026-03-11 15:01:56 -0500

    Update local storage path
    Updated local storage path to dev FQDN name.

--- ubiquitous/paths.py
+++ ubiquitous/paths.py
@@ -27,18 +27,18 @@ def getUserDataDir() -> Path:
   systemName = platform.system()
 
   if systemName == "Darwin":
-    return Path.home() / "Library" / "Application Support" / "com.psychocodermonkey.dev"
+    return Path.home() / "Library" / "Application Support" / "dev.psychocodermonkey"
 
   if systemName == "Windows":
     localAppData = os.getenv("LOCALAPPDATA")
     if localAppData:
       return Path(localAppData) / "com.psychocodermonkey.dev"
-    return Path.home() / "AppData" / "Local" / "com.psychocodermonkey.dev"
+    return Path.home() / "AppData" / "Local" / "dev.psychocodermonkey"
 
   xdgDataHome = os.getenv("XDG_DATA_HOME")
   if xdgDataHome:
     return Path(xdgDataHome) / "com.psychocodermonkey.dev"
-  return Path.home() / ".local" / "share" / "com.psychocodermonkey.dev"
+  return Path.home() / ".local" / "share" / "dev.psychocodermonkey"
 
 
 def _enforceGitignore(dirPath: Path) -> None:
